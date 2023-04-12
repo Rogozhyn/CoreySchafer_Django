cd9d5db6b83fe26e2aa5865673d7eca698bd370b
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-04-12 15:26:40 +0300

    Login - Logout

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
+LOGIN_REDIRECT_URL = 'blog-home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
