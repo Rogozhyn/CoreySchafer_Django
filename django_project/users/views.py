508aa09b9d44a8630e78f95b06d00d59888149fc
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-04-14 17:39:55 +0300

    Fixed user registration with profile creation

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -9,6 +9,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
             messages.success(request, 'Your account has been created! You are able to log in')
             return redirect('login')
     else:
